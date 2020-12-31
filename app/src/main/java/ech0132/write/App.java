ab3b51cfb033dd2f0151754521fb07f5c9d6e2c2
Stefan Ziegler <edi.gonzales@gmail.com>
2020-12-31 18:39:31 +0100

    xjc done

---
+++ app/src/main/java/ech0132/write/App.java
@@ -0,0 +1,11 @@
+package ech0132.write;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
